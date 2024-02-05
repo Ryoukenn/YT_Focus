db98a2bfd8af95b900893eeb2ee27c7175c60eec
Xavier No <dr.xavierngo@gmail.com>
2024-02-05 10:33:06 -0600

    All toggles in, semi-working
    Need to add different script for different pages and the query for the side and comment still returns null.

--- content.js
+++ content.js
@@ -1,24 +1,101 @@
-// Retrieve the toggle state from storage
-chrome.storage.sync.get(["toggleState"], function (result) {
-  var home = document.getElementById("primary");
-  console.log(home);
-  // Perform actions based on the stored toggle state
-  if (result.toggleState) {
-    home.style.display = "none";
-  } else {
-    home.style.display = "";
-  }
-});
+(function (window, document, undefined) {
+  // code that should be taken care of right away
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  var home = document.getElementById("primary");
-  console.log(home);
-  if (request.toggle) {
-    // Show elements or perform any action when the toggle is ON
+  window.onload = init;
 
-    home.style.display = "none";
-  } else {
-    // Hide elements or perform any action when the toggle is OFF
-    home.style.display = "";
+  function init() {
+    // the code to be called when the dom has loaded
+    // #document has its nodes
+    // Retrieve the toggle state from storage
+    chrome.storage.sync.get(
+      ["toggleState1", "toggleState2", "toggleState3", "toggleState4"],
+      function (result) {
+        var home =
+          '<div id="primary" class="style-scope ytd-two-column-browse-results-renderer" style="display: none;"></div>';
+        var shorts = document.querySelector('[title="Shorts"]');
+        var side = document.querySelector('[id="secondary"]');
+        var comment = document.querySelector(
+          '[section-identifier="comment-item-section"]'
+        );
+        console.log("hello!!");
+        console.log(home);
+        console.log(shorts);
+        console.log(side);
+        console.log(comment);
+        // Perform actions based on the stored toggle state
+        if (result.toggleState1) {
+          home.style.display = "none";
+        } else {
+          home.style.display = "block";
+        }
+
+        // Perform actions based on the stored toggle state
+        if (result.toggleState2) {
+          shorts.style.display = "none";
+        } else {
+          shorts.style.display = "block";
+        }
+
+        // Perform actions based on the stored toggle state
+        if (result.toggleState3) {
+          side.style.display = "none";
+        } else {
+          side.style.display = "block";
+        }
+
+        // Perform actions based on the stored toggle state
+        if (result.toggleState4) {
+          comment.style.display = "none";
+        } else {
+          comment.style.display = "block";
+        }
+      }
+    );
+
+    chrome.runtime.onMessage.addListener(function (
+      request,
+      sender,
+      sendResponse
+    ) {
+      var home = document.querySelector(
+        '[id="primary"][class="style-scope ytd-two-column-browse-results-renderer"]'
+      );
+      var shorts = document.querySelector('[title="Shorts"]');
+      var side = document.querySelector('[id="secondary"]');
+      var comment = document.querySelector(
+        '[section-identifier="comment-item-section"]'
+      );
+      if (request.toggle) {
+        // Show elements or perform any action when the toggle is ON
+        console.log(request.key + " is ON");
+        if (request.key === "toggleState1") {
+          home.style.display = "none";
+        }
+        if (request.key === "toggleState2") {
+          shorts.style.display = "none";
+        }
+        if (request.key === "toggleState3") {
+          side.style.display = "none";
+        }
+        if (request.key === "toggleState4") {
+          comment.style.display = "none";
+        }
+      } else {
+        // Hide elements or perform any action when the toggle is OFF
+        console.log(request.key + " is OFF");
+        if (request.key === "toggleState1") {
+          home.style.display = "block";
+        }
+        if (request.key === "toggleState2") {
+          shorts.style.display = "block";
+        }
+        if (request.key === "toggleState3") {
+          side.style.display = "block";
+        }
+        if (request.key === "toggleState4") {
+          comment.style.display = "block";
+        }
+      }
+    });
   }
-});
+})(window, document, undefined);
--- popup.js
+++ popup.js
@@ -1,21 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the toggle switch element
-  var toggleSwitch = document.getElementById("toggleSwitch");
-
-  // Retrieve the toggle state from storage and update the switch
-  chrome.storage.sync.get(["toggleState"], function (result) {
-    toggleSwitch.checked = result.toggleSwitch || false;
-  });
-
+  // Get the toggle switch 1 element
+  var toggleSwitch1 = document.getElementById("toggleSwitch1");
   // Add event listener for the toggle switch
-  toggleSwitch.addEventListener("change", function () {
-    // Save the toggle state to storage
-    chrome.storage.sync.set({ toggleState: toggleSwitch.checked });
-
-    // Get the active tab in the current window
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      // Send a message to the content script to toggle elements on the webpage
-      chrome.tabs.sendMessage(tabs[0].id, { toggle: toggleSwitch.checked });
-    });
+  toggleSwitch1.addEventListener("change", function () {
+    updateToggleState("toggleState1", toggleSwitch1.checked);
   });
+
+  // Get the toggle switch 2 element
+  var toggleSwitch2 = document.getElementById("toggleSwitch2");
+  // Add event listener for the toggle switch
+  toggleSwitch2.addEventListener("change", function () {
+    updateToggleState("toggleState2", toggleSwitch2.checked);
+  });
+
+  // Get the toggle switch 3 element
+  var toggleSwitch3 = document.getElementById("toggleSwitch3");
+  // Add event listener for the toggle switch
+  toggleSwitch3.addEventListener("change", function () {
+    updateToggleState("toggleState3", toggleSwitch3.checked);
+  });
+
+  // Get the toggle switch 4 element
+  var toggleSwitch4 = document.getElementById("toggleSwitch4");
+  // Add event listener for the toggle switch
+  toggleSwitch4.addEventListener("change", function () {
+    updateToggleState("toggleState4", toggleSwitch4.checked);
+  });
+
+  // Retrieve the toggle states from storage and update the switches
+  chrome.storage.sync.get(
+    ["toggleState1", "toggleState2", "toggleState3", "toggleState4"],
+    function (result) {
+      toggleSwitch1.checked = result.toggleState1 || false;
+      toggleSwitch2.checked = result.toggleState2 || false;
+      toggleSwitch3.checked = result.toggleState3 || false;
+      toggleSwitch4.checked = result.toggleState4 || false;
+    }
+  );
+  console.log("working");
 });
+
+function updateToggleState(key, state) {
+  // Save the toggle state to storage
+  var update = {};
+  update[key] = state;
+  chrome.storage.sync.set(update);
+
+  // Get the active tab in the current window
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // Send a message to the content script to toggle elements on the webpage
+    chrome.tabs.sendMessage(tabs[0].id, { key: key, toggle: state });
+  });
+}
