d0ffd36574ced6617651978b2f6b2cd83ccb0709
Xavier No <dr.xavierngo@gmail.com>
2024-02-06 18:58:59 -0600

    Added some css

--- content.js
+++ content.js
@@ -1,14 +1,12 @@
 const observer = new MutationObserver(() => {
-  // Your code to hide/show elements
   chrome.storage.sync.get(
     ["toggleState1", "toggleState2", "toggleState3", "toggleState4"],
     function (result) {
-      console.log(window.location.pathname);
+      //console.log(window.location.pathname);
 
       // Check if the current URL matches the pattern for Toggle 2
       if (window.location.pathname.startsWith("/watch")) {
-        // Code for Toggle 2
-        console.log("Toggle 2 is enabled on this page");
+        //console.log("Toggle 2 is enabled on this page");
         setTimeout(() => {
           var side = document.querySelector(
             '[id="secondary"][class="style-scope ytd-watch-flexy"]'
@@ -16,17 +14,15 @@ const observer = new MutationObserver(() => {
           var comment = document.querySelector(
             '[id="comments"][class="style-scope ytd-watch-flexy"]'
           );
-          console.log(side);
-          console.log(comment);
+          //console.log(side);
+          //console.log(comment);
 
-          // Perform actions based on the stored toggle state
           if (result.toggleState3) {
             side.style.display = "none";
           } else {
             side.style.display = "block";
           }
 
-          // Perform actions based on the stored toggle state
           if (result.toggleState4) {
             comment.style.display = "none";
           } else {
@@ -35,24 +31,21 @@ const observer = new MutationObserver(() => {
         }, 200);
       } // Check if the current URL matches the pattern for Toggle 1
       else {
-        // Code for Toggle 1
-        console.log("Toggle 1 is enabled on this page");
+        //console.log("Toggle 1 is enabled on this page");
 
         setTimeout(() => {
           var home = document.querySelector('[id="primary"]');
 
           var shorts = document.querySelector('[title="Shorts"]');
-          console.log(home);
-          console.log(shorts);
+          //console.log(home);
+          //console.log(shorts);
 
-          // Perform actions based on the stored toggle state
           if (result.toggleState1) {
             home.style.display = "none";
           } else {
             home.style.display = "block";
           }
 
-          // Perform actions based on the stored toggle state
           if (result.toggleState2) {
             shorts.style.display = "none";
           } else {
@@ -78,7 +71,7 @@ const observer = new MutationObserver(() => {
     );
     if (request.toggle) {
       // Show elements or perform any action when the toggle is ON
-      console.log(request.key + " is ON");
+      //console.log(request.key + " is ON");
       if (request.key === "toggleState1") {
         home.style.display = "none";
       }
@@ -93,7 +86,7 @@ const observer = new MutationObserver(() => {
       }
     } else {
       // Hide elements or perform any action when the toggle is OFF
-      console.log(request.key + " is OFF");
+      //console.log(request.key + " is OFF");
       if (request.key === "toggleState1") {
         home.style.display = "block";
       }
